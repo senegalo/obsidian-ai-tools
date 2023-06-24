2aad33f5ee737daf7a77795cdb92b937b0750634
Karim Mansour <kmansour@spotify.com>
2023-06-24 20:03:45 +0200

    Initial Plugin

--- main.ts
+++ main.ts
@@ -1,81 +1,38 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Configuration, OpenAIApi } from "openai";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface AIToolsSettings {
+	openaiAPIKey: string;
+	randomness: number
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: AIToolsSettings = {
+	openaiAPIKey: '',
+	randomness: 1
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: AIToolsSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'ai-summarise-note',
+			name: 'Summarise a Note using AI',
+			editorCallback: async (editor: Editor) => {
+				const summary = await this.summarize(editor.getValue())
+				const lineCount = editor.lineCount();
+				console.log(summary);
+				editor.replaceRange(summary, {line: lineCount, ch: 0})
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new AIToolsSettingsTab(this.app, this));
 	}
 
 	onunload() {
@@ -89,25 +46,31 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async summarize(content: string): Promise<string> {
+		const prompt = "summarize the following notes into a series of points. The output format should just be summary in markdown format: \n" + content;
+		const openai = this.loadOpenAI();
+		return openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			messages: [{ role: "user", content: prompt }],
+		}).then((r) => {
+			console.log(r);
+			return "\n\n---\n\n## Summary\n\n"+r.data.choices.first()?.message?.content
+		}).catch(e => {
+			console.error(e);
+			return "";
+		});
+	}
+
+	loadOpenAI() {
+		const configuration = new Configuration({
+			apiKey: this.settings.openaiAPIKey,
+		});
+		return new OpenAIApi(configuration);
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class AIToolsSettingsTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -123,14 +86,26 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Openai API Token')
+			.setDesc('OpenAI API Token from the settings')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.openaiAPIKey)
 				.onChange(async (value) => {
 					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.openaiAPIKey= value;
+					await this.plugin.saveSettings();
+				}));
+		
+		new Setting(containerEl)
+			.setName('Radmoness of resrultes (temprature)')
+			.setDesc('The reandomess of the repeated similar prompts')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.openaiAPIKey)
+				.onChange(async (value) => {
+					console.log('Secret: ' + value);
+					this.plugin.settings.openaiAPIKey= value;
 					await this.plugin.saveSettings();
 				}));
 	}
